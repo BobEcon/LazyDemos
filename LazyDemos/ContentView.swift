faa52ab4d835e570f0883a4b8575ff29de4bfa48
Robert Beachill <bbeachill@icloud.com>
2025-03-21 17:51:54 +0000

    Initial Commit

---
+++ LazyDemos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyDemos
+//
+//  Created by Robert Beachill on 21/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Robert Beachill on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
